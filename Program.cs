3dd1ab7c2de769450f815110a8b256a0badf9481
Tonroad <tonroad.map@web.de>
2025-07-08 11:09:27 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,31 +3,33 @@ using TonRoadVoteApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// PostgreSQL
+// PostgreSQL (подключение из переменной окружения Railway)
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+	options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Apply EF Core migrations
-PrepDb.PrepPopulation(app);
-
-// Swagger
+// Swagger (доступен всегда)
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// HTTPS
+// Разрешаем HTTPS-редирект (не мешает Railway)
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
 
-// Railway port bind
+// Указываем порт для Railway
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://*:{port}");
+
+// ВАЖНО: применяем миграции автоматически
+PrepDb.PrepPopulation(app);
 
 app.Run();
