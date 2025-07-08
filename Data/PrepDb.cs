99b23954f5db7b25df485b4446cd88f310b815f2
Tonroad <tonroad.map@web.de>
2025-07-08 11:23:01 +0200

    Update PrepDb.cs

--- Data/PrepDb.cs
+++ Data/PrepDb.cs
@@ -1,19 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace TonRoadVoteApi.Data
-{
-    public static class PrepDb
-    {
-        public static void PrepPopulation(IApplicationBuilder app)
-        {
-            using var serviceScope = app.ApplicationServices.CreateScope();
-            SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
-        }
+namespace TonRoadVoteApi.Data;
 
-        private static void SeedData(AppDbContext context)
-        {
-            Console.WriteLine("--> Applying Migrations...");
-            context.Database.Migrate();
-        }
+public static class PrepDb
+{
+    public static void PrepPopulation(WebApplication app)
+    {
+        using var serviceScope = app.Services.CreateScope();
+        var context = serviceScope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        Console.WriteLine("--> Applying Migrations...");
+        context.Database.Migrate();
     }
 }
