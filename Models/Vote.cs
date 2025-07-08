2dbbe6fa5f31ad05ba6abcc62c03dd966ede36bd
Tonroad <tonroad.map@web.de>
2025-07-08 11:01:30 +0200

    Update Vote.cs

--- Models/Vote.cs
+++ Models/Vote.cs
@@ -1,12 +1,12 @@
-﻿using System;
+using System;
 
 namespace TonRoadVoteApi.Models
 {
-	public class Vote
-	{
-		public int Id { get; set; }
-		public string UserId { get; set; }
-		public string Direction { get; set; }
-		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-	}
+    public class Vote
+    {
+        public int Id { get; set; }
+        public string? UserId { get; set; }
+        public string? Direction { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
 }
